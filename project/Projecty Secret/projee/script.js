e097f1f1f09530bddce5c2a20676a9626b308047
Welington19 <113564621+Welington19@users.noreply.github.com>
2023-03-23 11:27:09 -0300

    Add files via upload

---
+++ project/Projecty Secret/projee/script.js
@@ -0,0 +1,42 @@
+var btn = document.querySelector(".no");
+var position
+
+btn.addEventListener("click", function() {
+    position ? (position = 0) : (position = 150);
+    btn.style.transform = `translate(${position}px,0px)`;
+    btn.style.transition = "all 0.1s ease";
+});
+
+var btn = document.querySelector(".no");
+var position
+
+btn.addEventListener("mouseover", function() {
+    position ? (position = 0) : (position = 150);
+    btn.style.transform = `translate(${position}px,0px)`;
+    btn.style.transition = "all 0.1s ease";
+});
+
+// function tanks() {
+//     alert(" :)");
+// } 
+
+
+var imagem1 = document.getElementById("yes-image")
+
+function tanks(){
+    return imagem1.innerHTML = "<img width='315' src = './Img/IMG-20230322-WA0013.jpg'>"
+
+}
+
+var imagem2 = document.getElementById(no-image)
+
+function not(){
+
+    function not(){
+        return imagem2.innerHTML = "<img width='345' src = 'https://s3.sa-east-1.amazonaws.com/content.prxadvogados.com.br/39ec0107fec84eada08ec90f33391a8c.jpg'>"
+    
+    }
+
+
+
+}
